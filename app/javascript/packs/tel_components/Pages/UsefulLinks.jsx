9393dc10d8cdca72ec8496107453df61da35cfbd
Igor <siafin2010@gmail.com>
2018-03-14 09:31:59 +0200

    a

--- app/javascript/packs/tel_components/Pages/UsefulLinks.jsx
+++ app/javascript/packs/tel_components/Pages/UsefulLinks.jsx
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types'
 
 export const UsefulLinks = (props) => {
 	return(
-
-
 		<ul className="container-fluid">
 			<li className="row"> <div className="col-sm-auto input-group-text" >UsefulLinks:</div> </li>
 			<li className="row useful-link"> 
-				<div className="col-sm-auto" ><a href="/admin/cards">Cards management</a></div> 
+				<div className="col-sm-auto" >
+				</div> 
 			</li>
-
 		</ul>
-
 	)
 }
